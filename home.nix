c0e224c132a719c8787f82fa5eaf7b9ac7fcaee8
Finn Hollenbeck <hollenbeckfl@tutanota.com>
2024-11-08 18:08:07 +0100

    Some fixes some changes, added more plugins to nvim

--- home.nix
+++ home.nix
@@ -30,7 +30,12 @@
     userName = "Finn Hollenbeck";
   };
 
-  programs.neovim = {
+  programs.neovim = 
+  let
+    toLua = str: "lua << EOF\n${str}\nEOF\n";
+    toLuaFile = file: "lua << EOF\n${builtins.readFile file}\nEOF\n";
+  in
+  {
     enable = true;
 
     viAlias = true;
@@ -38,10 +43,29 @@
     vimdiffAlias = true;
 
     plugins = with pkgs.vimPlugins; [
-        {
-            plugin = gruvbox-nvim;
-            config = "colorscheme gruvbox";
-        }
+      {
+        plugin = gruvbox-nvim;
+        config = "colorscheme gruvbox";
+      }
+
+      {
+        plugin = telescope-nvim;
+        config = toLuaFile ./nvim/plugin/telescope.lua;
+      }
+
+      telescope-fzf-native-nvim
+
+      {
+        plugin = (nvim-treesitter.withPlugins (p: [
+          p.tree-sitter-nix
+          p.tree-sitter-vim
+          p.tree-sitter-bash
+          p.tree-sitter-lua
+          p.tree-sitter-python
+          p.tree-sitter-json
+        ]));
+        config = toLuaFile ./nvim/plugin/treesitter.lua;
+      }
     ];
 
     extraLuaConfig = ''
